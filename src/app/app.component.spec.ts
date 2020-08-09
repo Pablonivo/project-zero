7a9c16fcac9c297b4c17ada4d71d809e97725625
Pablonivo <ivokok1993@gmail.com>
2020-08-09 11:21:31 +0200

    Changed template to my own needs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('project-zero app is running!');
+    expect(compiled.querySelector('.content span').textContent).toBe('Welcome to the project-zero space app!');
   });
 });
