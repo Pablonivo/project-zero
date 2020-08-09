3a067c230d747f00cb68861e7421ee762528a6a1
Pablonivo <ivokok1993@gmail.com>
2020-08-09 15:48:44 +0200

    Refactored titles of problems to reduce clutter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,15 @@ export class AppComponent {
   }
 
   title = 'project-zero';
+  problemIdTitleDictionary: ProblemTitle[] = [
+    {id: 1, title: 'Multiples of 3 and 5'},
+    {id: 2, title: 'Even Fibonacci numbers'},
+    {id: 3, title: 'Largest prime factor'},
+    {id: 4, title: 'Largest palindrome product'}
+  ]
+}
+
+export class ProblemTitle {
+  id: number;
+  title: string;
 }
